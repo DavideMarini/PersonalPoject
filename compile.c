73d19067a71db1ded6b62f9ddcd009fa5d92b039
DavideMarini <davide.marinig@gmail.com>
2025-06-02 21:19:00 +0200

    testare compile.c in modo da poter compilare agilmente un progetto

---
+++ compile.c
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+
+int main(){
+	
+	system("gcc -std=c90 -O0 -g -Wall -Wextra -Wpedantic -Wconversion pedantic.c -o pedantic");
+	
+	
+}
---
+++ pedantic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+	
+	unsigned char* buffer = (unsigned char*)malloc(64);
+	memset(buffer, 0, 64);
+	
+	*((int*)(buffer)) = 58944;
+	
+	printf("%X\n", *((int*)(buffer)));
+	
+	printf("%X\n", (*buffer));
+	printf("%X\n", *(buffer+1));
+	printf("%X\n", *(buffer+2));
+	printf("%X\n", *(buffer+3));
+	
+	free(buffer);
+	
+    return 0;
+}
